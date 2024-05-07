c5c44d079ae1cae91f3da13535e99e7470258fae
Timur <timhod06@gmail.com>
2024-05-07 14:43:42 +0300

    Fix ntpTime package

--- shadowaead_2022/service.go
+++ shadowaead_2022/service.go
@@ -79,7 +79,7 @@ func NewService(method string, psk []byte, udpTimeout int64, handler shadowsocks
 			cache.WithAge[uint64, *serverUDPSession](udpTimeout),
 			cache.WithUpdateAgeOnGet[uint64, *serverUDPSession](),
 		),
-		ntpClient := ntpTime.NewNTPClient("pool.ntp.org"),
+		ntpClient: ntpTime.NewNTPClient("pool.ntp.org"),
 	}
 
 	s.ntpClient.UpdateTime()
