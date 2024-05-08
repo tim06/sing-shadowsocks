aaa7b39137540ae8435016e739cb2bde218f800a
Timur <timhod06@gmail.com>
2024-05-08 09:14:00 +0300

    Fix ntpTime package

--- ntp/ntpTime.go
+++ ntp/ntpTime.go
@@ -7,47 +7,50 @@ import (
 )
 
 type NTPClient struct {
- ntpServer   string
- offset      time.Duration
- mutex       sync.Mutex
- lastSynced  time.Time
+ ntpServer    string
+ offset       time.Duration
+ mutex        sync.Mutex
+ lastSynced   time.Time
+ synced       bool
 }
 
-func NewNTPClient(ntpServer string) (*NTPClient, error) {
+func NewNTPClient(ntpServer string) *NTPClient {
  client := &NTPClient{
   ntpServer: ntpServer,
  }
-
- if err := client.syncTime(); err != nil {
-  return nil, err
- }
-
- return client, nil
+ client.UpdateTime()
+ return client
 }
 
-func (c *NTPClient) syncTime() error {
+func (c *NTPClient) UpdateTime() {
  c.mutex.Lock()
  defer c.mutex.Unlock()
 
  response, err := ntp.Query(c.ntpServer)
  if err != nil {
-  return err
+  c.synced = false
+  return
  }
-
  err = response.Validate()
  if err != nil {
-  return err
+  c.synced = false
+  return
  }
-
  c.offset = response.ClockOffset
  c.lastSynced = time.Now()
-
- return nil
+ c.synced = true
 }
 
 func (c *NTPClient) Now() time.Time {
  c.mutex.Lock()
  defer c.mutex.Unlock()
+
+ if !c.synced {
+  c.UpdateTime()
+  if !c.synced {
+   return time.Now()
+  }
+ }
 
  return time.Now().Add(c.offset)
 }
