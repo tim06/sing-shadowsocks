fa933ee07838b7ac3a336a0141fa123d4d7dfd0b
Timur <timhod06@gmail.com>
2024-07-04 13:35:30 +0300

    Update ntpTime.go

--- ntp/ntpTime.go
+++ ntp/ntpTime.go
@@ -3,18 +3,18 @@ package ntp
 import (
     "github.com/beevik/ntp"
     "log"
-    "fmt"
     "sync"
     "sync/atomic"
     "time"
 )
 
 type NTPClient struct {
-    ntpServers []string
-    offset     time.Duration
-    mutex      sync.Mutex
-    lastSynced time.Time
-    synced     atomic.Bool
+    ntpServers       []string
+    offset           time.Duration
+    mutex            sync.Mutex
+    lastSynced       time.Time
+    synced           atomic.Bool
+    syncAttemptsDone atomic.Bool
 }
 
 func NewNTPClient(ntpServers ...string) *NTPClient {
@@ -29,30 +29,36 @@ func (c *NTPClient) UpdateTime() {
     c.mutex.Lock()
     defer c.mutex.Unlock()
 
+    if c.syncAttemptsDone.Load() {
+        return
+    }
+
     for _, server := range c.ntpServers {
         response, err := ntp.Query(server)
         if err != nil {
-            log.Println(fmt.Sprintf("Error querying NTP server %s: %v", server, err))
+            log.Printf("Error querying NTP server %s: %v", server, err)
             continue
         }
         err = response.Validate()
         if err != nil {
-            log.Println(fmt.Sprintf("Validation error from NTP server %s: %v", server, err))
+            log.Printf("Validation error from NTP server %s: %v", server, err)
             continue
         }
         c.offset = response.ClockOffset
         c.lastSynced = time.Now()
         c.synced.Store(true)
-        log.Println(fmt.Sprintf("Time synchronized with NTP server %s", server))
+        c.syncAttemptsDone.Store(true)
+        log.Printf("Time synchronized with NTP server %s", server)
         return
     }
 
     c.synced.Store(false)
-    log.Println(fmt.Sprintf("Failed to synchronize time with all provided NTP servers"))
+    c.syncAttemptsDone.Store(true)
+    log.Println("Failed to synchronize time with all provided NTP servers")
 }
 
 func (c *NTPClient) Now() time.Time {
-    if !c.synced.Load() {
+    if !c.synced.Load() && !c.syncAttemptsDone.Load() {
         go c.UpdateTime()
         if !c.synced.Load() {
             return time.Now()
